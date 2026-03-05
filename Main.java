e37645f9b1434c3f8cdd574764af741c3d955ec8
hayaboy77 <hayaboy77@gmail.com>
2026-03-05 11:12:57 +0900

    26_03_05_j_test

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{     
+
+   public static void main(String args []){
+
+        Animal dog =new Dog();
+        dog.sound();
+              
+        Animal cat =new Cat();
+        cat.sound();
+    }
+}
